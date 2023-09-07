fd15ca1d37277ff9d43b8d9f8742da58ddeedad3
Leonardo Santos <leonardojdossantos@gmail.com>
2023-09-06 23:38:27 -0300

    chore: add prisma with basic schema

---
+++ prisma/migrations/20230906233000_create_account_and_profile_table/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE `Account` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Profile` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `avatarUrl` VARCHAR(191) NULL,
+    `accountId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Profile_accountId_key`(`accountId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Profile` ADD CONSTRAINT `Profile_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
